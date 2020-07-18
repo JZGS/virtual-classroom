381f7e53601aad24e523100b4740abe899d01c92
JZGS <jcarlosgs1316@gmail.com>
2020-07-17 20:18:18 -0500

    Project basic structure created. Dependencies added

---
+++ src/test/java/jc/sw/dev/virtualclassroom/VirtualClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package jc.sw.dev.virtualclassroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualClassroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
